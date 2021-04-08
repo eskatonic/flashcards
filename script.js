de1e735ef681ed36182963ecb206cc969d306476
Brian Mays <brianmays@me.com>
2021-04-08 12:54:23 -0700

    Commit 1
    First commit - initial file and structure tests.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+// VARIABLES
+const rightButton = document.querySelector("#right");
+const wrongButton = document.querySelector("#wrong");
+
+
+// FUNCTIONS
+
+// SHUFFLE DECK:
+// Create a new array of cards from the deck.
+const newCard = (e) => {
+    console.log("NEW CARD BUTTON PRESSED");
+
+    // 
+}
+
+const wrongButtonPress = (e) => {
+    console.log("WRONG BUTTON PRESSED");
+}
+
+// EVENT HANDLERS
+rightButton.addEventListener("click", newCard);
+wrongButton.addEventListener("click", wrongButtonPress);
+
