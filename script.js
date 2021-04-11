0db8138290fa5376136aa7a3f939344ac95af24e
Brian Mays <brianmays@me.com>
2021-04-11 14:54:32 -0700

    Flashcard is now pulling data from an object

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 
+
+// Get Spanish card data from JSON and publish
+// When button is clicked, card "flips" to English version.
+// Card flips back when clicked again.
+// If "Correct" is pressed, correct count goes up by 1 and card is discarded.
+// If "Wrong" is pressed, card is pushed to end of deck for retry.
+
+
 // VARIABLES
 const rightButton = document.querySelector("#right");
 const wrongButton = document.querySelector("#wrong");
+const cardButton = document.querySelector("#card");
+const nextButton = document.querySelector("#next");
+// let cards = "CARD TEXT HERE"; <- just used to test card text area
+let cards = [{"id:": 1, "Spanish": "La Calle", "English": "Street"}];
+
 // import data from "./cards.json";
 // console.log(data);
 
@@ -17,15 +30,24 @@ function load() {
 // Create a new array of cards from the deck.
 const newCard = (e) => {
     console.log("NEW CARD BUTTON PRESSED");
-
-    // 
 }
 
 const wrongButtonPress = (e) => {
     console.log("WRONG BUTTON PRESSED");
 }
 
+const cardPress = (e) => {
+    e.target.innerText = `${cards[0].Spanish}`;
+    //console.log(`${cards."English"}`);
+}
+
+const nextButtonPress = (e) => {
+    console.log("NEXT CARD BUTTON PRESSED");
+}
+
 // EVENT HANDLERS
 rightButton.addEventListener("click", newCard);
 wrongButton.addEventListener("click", wrongButtonPress);
+cardButton.addEventListener("click", cardPress);
+nextButton.addEventListener("click", nextButtonPress);
 
